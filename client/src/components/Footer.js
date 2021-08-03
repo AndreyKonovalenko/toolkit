f2d889b5b4b1ff87e71773e17761771671016ab3
AndreyKonovalenko <andreinew@mail.ru>
2021-08-03 22:04:40 +0300

    chore: footer style

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { createUseStyles, useTheme } from 'react-jss';
+import {NavLink} from 'react-router-dom';
+import {createUseStyles, useTheme} from 'react-jss';
 
 let useStyles = createUseStyles((theme) => ({
-  container: { background: theme.colorTertiary },
+  container: {background: theme.colorTertiary},
   footer: {
     border: '1px, solid, #e7e7e7',
     backgroundColor: '#f3f3f3',
@@ -12,18 +12,17 @@ let useStyles = createUseStyles((theme) => ({
     color: '#666',
   },
   small: {
-    fontWeight: 400
-  }
+    fontWeight: 400,
+  },
 }));
 
 const NavBar = (props) => {
   const theme = useTheme();
-  const classes = useStyles({ ...props, theme });
-
+  const classes = useStyles({...props, theme});
 
   return (
     <div className={classes.footer}>
-      <small className={classes.small}>Created by Andrew</small>
+      <small className={classes.small}>Created by AK</small>
     </div>
   );
 };
