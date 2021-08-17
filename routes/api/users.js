b737fac460ad9774d29c3457a3ae031075c7b266
EC2 Default User <andreinew@mail.ru>
2021-08-17 08:48:47 +0000

    chore: work on users route logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult} = require("express-validator");
 
-// route: POST api/user/register
-// desc: Register new user
-// access Public
+const User = require('../../models/User');
 
-router.post('/',(req, res)=> {
-  console.log(req.body);
-  res.send("User route");
+//@desc Test users route
+//@rote GET api/users/test
+//@access Public
+
+router.get('/test', (req, res) => res.json({ msg: 'User route works' }));
+
+//@desc Register new user
+//@route POST api/users
+//@acces Public
+
+router.post(
+  '/',
+  check('name', 'Name is required!').not().isEmpty(),
+  check('email', 'Please include a valid email!').isEmail(),
+  // password must be at least 5 chars log
+  check('password', 'Please enter a password with 5 or more characters!').isLength({min: 5}),
+  async (req, res)=> {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      return res.status(400).json({erros: errors.array()});
+    }
+
+    const {name, email, password} = req.body;
+
+    try {
+
+    // See if user exists
+
+    // Get user gravatar
+
+    // Encrypt password
+
+    // Return jsonwebtoken
+
+    } catch (err) {
+
+    }
+
+    console.log(req.body);
+    res.send("User route");
 });
+
+
+
+
 
 module.exports = router;
