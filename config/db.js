7803b0b1ae90bb81c29287a4dc825728704e36b3
EC2 Default User <andreinew@mail.ru>
2021-08-19 12:58:23 +0000

    chore: fix issue with mongoose connect logic

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const db = require('./default').mongoURI;
 
-const connectDB = () => {
-  mongoose
-    .createConnection(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log(`MongoDB Connected!`))
-    .catch((err) => console.log(err));
+const connectDB = async () => {
+  try{
+    await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+    console.log(`MongoDB Connected!`);
+  } catch (err) {
+    console.error(err.massage);
+    // Exit process with failure
+    process.exit(1);
+  }
 };
-
 
 
 module.exports = connectDB;
