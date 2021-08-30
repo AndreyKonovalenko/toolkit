0e0066dca2956e6fc764b478d8594924fa742543
EC2 Default User <andreinew@mail.ru>
2021-08-30 14:06:31 +0000

    chore: work on redux store implamentation

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,21 @@ import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from  './store/store';
+
+
 const App = () => {
   const routing = useRoutes(routes);
 
   return (
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       {routing}
     </ThemeProvider>
+    </Provider>
   );
 };
 
